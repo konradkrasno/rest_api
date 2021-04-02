641b4f87341a30e6097dabc8be8a75a861e61209
konradkrasno <konradkrasno@gmail.com>
2021-04-02 09:14:00 +0200

    Add sudo to docker commands

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker build . -t rest_api:latest
-docker run --name rest_api -p 8000:8000 -d --rm rest_api
+sudo docker build . -t rest_api:latest
+sudo docker run --name rest_api -p 8000:8000 -d --rm rest_api
