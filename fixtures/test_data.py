881822dfc44c95d393c83d9cbf8c6f01794f1dc6
konradkrasno <konradkrasno@gmail.com>
2021-04-02 09:25:47 +0200

    Add one more person to test data

--- fixtures/test_data.py
+++ fixtures/test_data.py
@@ -2,12 +2,14 @@ input_data = {
     "data_list": [
         {"first_name": "Stefan", "second_name": "Nowak", "birth_date": "1988-06-18"},
         {"first_name": "Jan", "second_name": "Kowalski", "birth_date": "1977-11-10"},
+        {"first_name": "Andrzej", "second_name": "Nowak", "birth_date": "1955-11-10"},
     ]
 }
 
 sorted_data = {
     "data_list": [
         {"first_name": "Jan", "second_name": "Kowalski", "birth_date": "1977-11-10"},
+        {"first_name": "Andrzej", "second_name": "Nowak", "birth_date": "1955-11-10"},
         {"first_name": "Stefan", "second_name": "Nowak", "birth_date": "1988-06-18"},
     ]
 }
@@ -19,6 +21,12 @@ result = {
             "second_name": "Kowalski",
             "birth_date": "1977-11-10",
             "hash": "d371d82da0bcb79c71d8a9d56a5272732f01ed3496887620c6f10ef9fa823e3a",
+        },
+        {
+            "first_name": "Andrzej",
+            "second_name": "Nowak",
+            "birth_date": "1955-11-10",
+            "hash": "8595ba77213f1a7df3579db7308f321bd309ce82e007697ce75014c64b3b0279",
         },
         {
             "first_name": "Stefan",
